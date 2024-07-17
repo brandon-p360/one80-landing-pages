3f9fe7307e95c9f2cf5984465b0530d6252df3db
brandon-p360 <115641186+brandon-p360@users.noreply.github.com>
2024-07-17 12:55:55 -0400

    [MOD] Setup first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        inlineStylesheets: "always",
+    },
+    vite: {
+        build: {
+            cssCodeSplit: false,
+        },
+        css: {
+            modules: {
+                localsConvention: "camelCase",
+            },
+        },
+    },
+});
